9132b85b7cca99fc7012466b1a3cc6edb8f67a47
Damien Menages <menages.damien@gmail.com>
2021-04-28 14:59:59 +0200

    review done

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
   validates :content, presence: true
-  validates :rating, presence: true
+  validates :rating, numericality: { only_integer: 0.5 }
+  validates :rating, :inclusion => 0..5
 end
